c4db79be6f6077331d4eea86982630a7c6b358bc
Sam Willis <sam.willis@hotmail.co.uk>
2016-11-17 08:05:55 +0000

    finished 6!

--- c3.py
+++ c3.py
@@ -2,10 +2,39 @@ import binascii
 from c2 import XORbytes
 import sortedcontainers
 from collections import defaultdict
+from Crypto.Util.strxor import strxor_c
 
-letterfreq = defaultdict(int, {' ':18, 'e':10, 't':8, 'a':7, 'o':6, 'n':6})
+freqs = {
+    'a': 0.0651738,
+    'b': 0.0124248,
+    'c': 0.0217339,
+    'd': 0.0349835,
+    'e': 0.1041442,
+    'f': 0.0197881,
+    'g': 0.0158610,
+    'h': 0.0492888,
+    'i': 0.0558094,
+    'j': 0.0009033,
+    'k': 0.0050529,
+    'l': 0.0331490,
+    'm': 0.0202124,
+    'n': 0.0564513,
+    'o': 0.0596302,
+    'p': 0.0137645,
+    'q': 0.0008606,
+    'r': 0.0497563,
+    's': 0.0515760,
+    't': 0.0729357,
+    'u': 0.0225134,
+    'v': 0.0082903,
+    'w': 0.0171272,
+    'x': 0.0013692,
+    'y': 0.0145984,
+    'z': 0.0007836,
+    ' ': 0.1918182 
+}
 
-char_by_score = sortedcontainers.SortedListWithKey(key=lambda val: val[2])
+letterfreq = defaultdict(float, freqs)
 
 def score(byte_array):
     score = 0
@@ -15,19 +44,19 @@ def score(byte_array):
     return score
 
 def main():
-    hexstr = "1b37373331363f78151b7f2b7834313_33d78397828372d363c78373e783a393b3736"
-    print(singleByteXOR_decrypt(hexstr)[0])
-
-def singleByteXOR_decrypt(hexstr):
+    hexstr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
     byte_array = binascii.a2b_hex(hexstr)
+    print(singleByteXOR_decrypt(byte_array))
+
+def singleByteXOR_decrypt(byte_array):
+    char_by_score = sortedcontainers.SortedListWithKey(key=lambda val: val[2])
     length = len(byte_array)
     for i in range(256):
         b2 = bytes([i])*length
         XORattempt = XORbytes(byte_array, b2)
         scr = score(XORattempt)
-        char_by_score.add((XORattempt, i, scr),)
+        char_by_score.add((XORattempt, chr(i), scr),)
     return char_by_score.pop()
-
 
 if __name__ == '__main__':
     main()
--- c6.py
+++ c6.py
@@ -1,4 +1,12 @@
 import binascii
+from sortedcontainers import SortedListWithKey
+from c3 import singleByteXOR_decrypt
+
+keysize_by_score = SortedListWithKey(key=lambda val: -val[1])
+
+bytes1 = b'this is a test'
+bytes2 = b'wokka wokka!!!'
+    
 
 def hammingDistance(byte_array1, byte_array2):
     if len(byte_array1) != len(byte_array2):
@@ -7,16 +15,28 @@ def hammingDistance(byte_array1, byte_array2):
     hammingDist_bytes = [bin(byte_array1[i]^byte_array2[i]).count("1") for i in range(length)]
     return sum(hammingDist_bytes)
 
-def xx(keysize):
-    
-
-
 def main():
-    for KEYSIZE in range(2:41):
-
-    bytes1 = b'this is a test'
-    bytes2 = b'wokka wokka!!!'
-    print(hammingDistance(bytes1, bytes2))
+    with open("6.txt", 'r') as file:
+        ciphertext = binascii.a2b_base64(file.read())
+        for keysize in range(2, 41):
+            score = 0
+            for i in range(int(len(ciphertext)/100)):
+                front_byte = ciphertext[i*keysize:(i+1)*keysize]
+                back_byte  = ciphertext[(i+1)*keysize:(i+2)*keysize]
+                score += hammingDistance(front_byte, back_byte)
+            norm_score = score/keysize
+            keysize_by_score.add((keysize, norm_score),)
+        for _ in range(1):
+            plaintext = [None]*len(ciphertext)
+            keysize = keysize_by_score.pop()[0]
+            key = [None]*keysize
+            for i in range(keysize):
+                cipherslice = ciphertext[i::keysize]
+                temp = singleByteXOR_decrypt(cipherslice)
+                plaintext[i::keysize] = temp[0]
+                key[i] = temp[1]
+            print(''.join(chr(c) for c in plaintext))
+            print("the key was: " + ''.join(key))
 
 if __name__ == '__main__':
     main()
