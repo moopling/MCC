e17efeb6af0878737f04089fd2d3e7aed69bf54e
Sam Willis <sam.willis@ttp.com>
2016-11-23 13:19:17 +0000

    finished 9, started 10 and 11

--- set 1/c1.py
+++ set 1/c1.py
@@ -1,12 +1,16 @@
 import binascii
 
+
 def hex2b64(hexstr):
     bytestr = binascii.a2b_hex(hexstr)
     b64str = binascii.b2a_base64(bytestr)
     return b64str
 
+
 def main():
-    hexstr = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'  
+    hexstr = ('49276d206b696c6c696e6720796f7572',
+              '20627261696e206c696b65206120706f',
+              '69736f6e6f7573206d757368726f6f6d',)
     b64str = hex2b64(hexstr)
     print(b64str)
 
--- set 2/c9.py
+++ set 2/c9.py
@@ -1,6 +1,7 @@
 def pad(plaintext: bytes, required_length: int) -> bytes:
-    padding = required_length-len(plaintext)
-    return plaintext + padding*bytes([padding])
+    padding = required_length - len(plaintext)
+    return plaintext + padding * bytes([padding])
+
 
 def main() -> None:
     key = b'YELLOW SUBMARINE'
