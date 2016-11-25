dbaae1aa1fd8856c8b296916928557fa9c0cf32d
Sam Willis <sam.willis@ttp.com>
2016-11-25 17:34:52 +0000

    ??

--- set 2/c11.py
+++ set 2/c11.py
@@ -1,16 +1,19 @@
 from Crypto.Cipher import AES
 from Crypto.Random import random
+from c10 import my_AES
 
 
 def encryption_oracle(plaintext: bytes) -> bytes:
-    plaintext = get_random_bytes(5, 10) + plaintext + get_random_bytes(5, 10)
+    padded_plaintext = get_random_bytes(
+        5, 10) + plaintext + get_random_bytes(5, 10)
     iv = get_random_bytes(16)
     key = get_random_bytes(16)
+    cipher = my_AES(key)
     if coinflip():
-        cipher = AES.AESCipher(key, AES.MODE_CBC, iv)
+        ciphertext = cipher.encrypt_ecb(padded_plaintext)
     else:
-        cipher = AES.AESCipher(key, AES.MODE_ECB)
-    return cipher.encrypt(plaintext)
+        ciphertext = cipher.encrypt_cbc(padded_plaintext, iv)
+    return ciphertext
 
 
 def get_random_bytes(start: int, stop: int=None) -> bytes:
@@ -22,16 +25,13 @@ def get_random_bytes(start: int, stop: int=None) -> bytes:
 
 
 def coinflip() -> bool:
-    if random.getrandbits(1):
-        return True
-    else:
-        return False
+    return bool(random.getrandbits(1))
 
 
 def main() -> None:
     message = b"hello"
-    encryption_oracle(message)
-    pass
+    encrypted_message = encryption_oracle(message)
+    print(encrypted_message)
 
 if __name__ == '__main__':
     main()
